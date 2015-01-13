3343497ff52bcecb9638ac745a70b3dab822ec15
mewpre <yichin.sun@gmail.com>
2015-01-13 14:08:19 -0800

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Yi-Chin Sun on 1/13/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Yi-Chin Sun on 1/13/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
